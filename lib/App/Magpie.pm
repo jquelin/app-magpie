10f181127833240723824007b4baa34bf78e5562
Jérôme Quelin <jquelin@gmail.com>
2011-01-24 14:41:21 +0100

    pod update

--- lib/App/Magpie.pm
+++ lib/App/Magpie.pm
@@ -3,10 +3,30 @@ use strict;
 use warnings;
 
 package App::Magpie;
-# ABSTRACT:
+# ABSTRACT: Mageia Perl Integration Easy
 
 1;
 __END__
+
+=head1 DESCRIPTION
+
+CPAN holds a lot of great modules - but it can be difficult for the user
+to install if she's not familiar with the process. Therefore, Linux
+distribution usually package quite a lot of them, for them to be easily
+installable.
+
+Mageia Linux is no exception, and ships more than 2500 packages holding
+Perl distributions (at the time of writing). Maintaining those packages
+is a daunting task - and cannot be done only by hand.
+
+This distribution is therefore a set of scripts helping maintaining Perl
+packages within Mageia. They can be somehow coupled or used
+independently.
+
+Even if they are Mageia-centered, and Perl-centered, some of those tools
+can be used also by to maintain non-Perl packages, or by other Linux
+distributions than Mageia. I'd like to hear from you in this case! :-)
+
 
 =head1 SEE ALSO
 
