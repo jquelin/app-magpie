76662ad2bbf02b1026714719e3e28b5c79167fa0
Jérôme Quelin <jquelin@gmail.com>
2011-01-24 14:23:01 +0100

    initial commit

---
+++ lib/App/Magpie.pm
@@ -0,0 +1,39 @@
+use 5.012;
+use strict;
+use warnings;
+
+package App::Magpie;
+# ABSTRACT:
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+You can find more information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Magpie>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Magpie>
+
+=item * Git repository
+
+L<http://github.com/jquelin/app-magpie>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Magpie>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Magpie>
+
+=back
+
+
