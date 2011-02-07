3d8d2b0a78542f1ecaa7b5e3a2810ead101b8d33
Jérôme Quelin <jquelin@gmail.com>
2011-02-07 13:04:48 +0100

    am:app:command now provides some std utils

--- lib/App/Magpie/App/Command.pm
+++ lib/App/Magpie/App/Command.pm
@@ -6,9 +6,60 @@ package App::Magpie::App::Command;
 # ABSTRACT: base class for sub-commands
 
 use App::Cmd::Setup -command;
+use Moose;
+use MooseX::Has::Sugar;
+
+use App::Magpie;
+
+
+# -- public attributes
+
+=attr magpie
+
+The L<App::Magpie> object responsible for the real operations.
+
+=cut
+
+has magpie => (
+    ro, lazy,
+    isa     => "App::Magpie",
+    default => sub { App::Magpie->new; }
+);
+
 
 # -- public methods
 
+=method log_init
+
+    $cmd->log_init($opts);
+
+Initializes the C<logger> attribute of C<magpie> depending on the
+value of verbose options.
+
+=cut
+
+sub log_init {
+    my ($self, $opts) = @_;
+    $self->magpie->logger->set_muted(1) if $opts->{quiet};
+    $self->magpie->logger->set_debug(1) if $opts->{verbose};
+}
+
+
+=method verbose_options
+
+    my @opts = $self->verbose_options;
+
+Return an array of verbose options to be used in a command's C<opt_spec>
+method. Those options can then be used by C<log_init()>.
+
+=cut
+
+sub verbose_options {
+    return (
+        [ 'verbose|v' => "display extra information" ],
+        [ 'quiet|q'   => "be quiet unless error"     ],
+    );
+}
 
 1;
 __END__
@@ -20,6 +71,6 @@ __END__
 
 =head1 DESCRIPTION
 
-This module is the base class for all sub-commands. It doesn't do
-anything special currently but trusting methods for pod coverage.
+This module is the base class for all sub-commands. It provides some
+methods to control logging.
 
