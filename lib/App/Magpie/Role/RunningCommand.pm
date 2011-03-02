4b3f670a7b8278391c0fc435ab748d2c0d332763
Jérôme Quelin <jquelin@gmail.com>
2011-03-02 11:08:03 +0100

    new role: running external command

---
+++ lib/App/Magpie/Role/RunningCommand.pm
@@ -0,0 +1,53 @@
+use 5.012;
+use strict;
+use warnings;
+
+package App::Magpie::Role::RunningCommand;
+# ABSTRACT: sthg that can run an external command
+
+use Moose::Role;
+use MooseX::Has::Sugar;
+
+with 'App::Magpie::Role::Logging';
+
+
+# -- public methods
+
+=method run_command
+
+    $obj->run_command( $cmd );
+
+Run a command, spicing some debug comments here and there. Die if the
+command encountered a problem.
+
+=cut
+
+sub run_command {
+    my ($self, $cmd) = @_;
+    my $logger = $self->logger;
+    $self->log_debug( "running: $cmd" );
+
+    my $stderr = $logger->log_level >= 2 ? "" : "2>/dev/null";
+
+    # run the command
+    system("$cmd $stderr >&2") == 0
+        or $self->log_fatal( [ "command [$cmd] exited with value %d", $?>>8] );
+}
+
+
+ 
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    with 'App::Magpie::Role::RunningCommand';
+    $self->run_command( "sleep 10" );
+
+
+=head1 DESCRIPTION
+
+This role is meant to provide easy way of running a command for classes
+consuming it. Standard output & standard errors are redirected depending
+on the log level.
+
