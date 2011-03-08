3e16042489e827001bf14fac5dcc906ff9c1b84b
Jérôme Quelin <jquelin@gmail.com>
2011-03-08 11:03:06 +0100

    old::module: new method category()

--- lib/App/Magpie/Action/Old/Module.pm
+++ lib/App/Magpie/Action/Old/Module.pm
@@ -9,6 +9,8 @@ use Moose;
 use MooseX::Has::Sugar;
 use MooseX::SemiAffordanceAccessor;
 
+use App::Magpie::URPM;
+
 
 # -- public attributes
 
@@ -24,16 +26,75 @@ The version of the module as currently installed.
 
 The module version, as available on CPAN.
 
-=attr package
+=attr packages
 
-The package holding the module.
+The Mageia packages holding the module (there can be more than one).
+Core packages (perl and perl-base) are excluded from this list.
+
+=attr is_core
+
+Whether the module is shipped in a core Perl package.
 
 =cut
 
-has name    => ( ro, isa => "Str", required );
-has oldver  => ( ro, isa => "Str" );
-has newver  => ( ro, isa => "Str" );
-has package => ( ro, isa => "Str" );
+has name     => ( ro, isa => "Str", required );
+has oldver   => ( ro, isa => "Str" );
+has newver   => ( ro, isa => "Str" );
+has is_core  => ( rw, isa => "Bool" );
+has packages => ( ro, isa => "ArrayRef", lazy_build, auto_deref );
+
+sub _build_packages {
+    my ($self) = @_;
+    my $urpm = App::Magpie::URPM->instance;
+    my $module = $self->name;
+    my @pkgs   = $urpm->packages_providing( $module );
+
+    $self->set_is_core( scalar( grep { $_->name =~ /^perl(-base)?$/ } @pkgs ) );
+    return [ grep { $_->name !~ /^perl(-base)?$/ } @pkgs ];
+}
+
+
+# -- public methods
+
+=method category
+
+    my $str = $module->category;
+
+Return the module category:
+
+=over 4
+
+=item * C<core> - one of the core packages (perl, perl-base)
+
+=item * C<dual-lifed> - core package + one other package
+
+=item * C<normal> - plain, non-core regular package
+
+=item * C<orphan> - installed package not shipped by a package
+(inherited from mandriva, or not yet submitted)
+
+=item * C<strange> - shipped by more than one non-core package
+
+=back
+
+=cut
+
+sub category {
+    my ($self) = @_;
+    my @pkgs   = $self->packages;
+    my $iscore = $self->is_core;
+
+    if ( $iscore ) {
+        return "core"       if scalar(@pkgs) == 0;
+        return "dual-lifed" if scalar(@pkgs) == 1;
+        return "strange"    if scalar(@pkgs) >= 2;
+    }
+
+    return "orphan"  if scalar(@pkgs) == 0;
+    return "normal"  if scalar(@pkgs) == 1;
+    return "strange" if scalar(@pkgs) >= 2;
+}
+
 
 __PACKAGE__->meta->make_immutable;
 1;
