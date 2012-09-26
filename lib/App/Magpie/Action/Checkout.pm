83e040e0b62f49535f635d505c32881fca950f3b
Your Name <you@example.com>
2012-09-26 14:07:56 +0200

    checkout: possibility to pass in a module name instead of pkg name

--- lib/App/Magpie/Action/Checkout.pm
+++ lib/App/Magpie/Action/Checkout.pm
@@ -9,6 +9,8 @@ use File::pushd;
 use Moose;
 use Path::Class;
 
+use App::Magpie::URPM;
+
 with 'App::Magpie::Role::Logging';
 with 'App::Magpie::Role::RunningCommand';
 
@@ -26,6 +28,17 @@ Return the directory in which the checkout is located.
 
 sub run {
     my ($self, $pkg, $directory) = @_;
+
+    # check if argument is a perl module or a mageia rpm name
+    if ( not _pkg_exist_in_svn($pkg) ) {
+        $self->log( "$pkg doesn't exist, looking if it's a perl module" );
+        my $urpm = App::Magpie::URPM->instance;
+        my ($realpkg) = map { $_->name } $urpm->packages_providing( $pkg );
+        $self->log_fatal( "$pkg doesn't exist and isn't a perl module, aborting" )
+            unless _pkg_exist_in_svn( $realpkg );
+        $self->log( "$pkg is a module provided by $realpkg" );
+        $pkg = $realpkg;
+    }
 
     # check out the package, or update the local checkout
     my $dir    = defined($directory) ? dir( $directory ) : dir();
@@ -45,6 +58,19 @@ sub run {
     return $pkgdir;
 }
 
+
+# -- private subs
+
+#
+#   my $bool = _pkg_exist_in_svn( $pkg );
+#
+# return true if $pkg is a real package in cauldron.
+#
+sub _pkg_exist_in_svn {
+    my $pkg = shift;
+    my $svn = "svn+ssh://svn.mageia.org/svn/packages/cauldron";
+    return system( "svn ls $svn/$pkg >/dev/null 2>&1" ) == 0;
+}
 
 
 1;
