6a7c87fb01ee85573d6972909e7350371ad112a1
Jerome Quelin <jquelin@gmail.com>
2014-09-30 14:40:04 +0200

    adding %{?perl_default_filter} (rpm 4.12)

--- lib/App/Magpie/Action/FixSpec.pm
+++ lib/App/Magpie/Action/FixSpec.pm
@@ -85,6 +85,9 @@ sub run {
     }
 
     # cleaning spec file
+    $self->log_debug( "adding %{?perl_default_filter}" );
+    $spec =~ s/^Name:/%{?perl_default_filter}\n\nName:/msi;
+
     $self->log_debug( "removing mandriva macros" );
     $spec =~ s/^%if %{mdkversion}.*?^%endif$//msi;
 
